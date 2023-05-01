7871f98511db78c6b00e570e8edbec528a3df72f
Leyla54 <brendlerleyla@gmail.com>
2023-05-01 12:18:39 +0200

    first commit

---
+++ barchart_distance_categories.py
@@ -0,0 +1,117 @@
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+from plotly.subplots import make_subplots
+
+df = pd.read_csv('https://gist.githubusercontent.com/florianeichin/cfa1705e12ebd75ff4c321427126ccee/raw/c86301a0e5d0c1757d325424b8deec04cc5c5ca9/flights_all_cleaned.csv', sep=',')
+small_df = pd.read_csv('https://gist.githubusercontent.com/florianeichin/b877d354d6bc52e6ce840572e40b0497/raw/19759410471073756a388dada5fcb40109f0d13e/flights_subset_cleaned.csv', sep=',')
+
+#------falls zeit ist noch total number of short,   flights hinschreiben und die ändert sich beim abwählen
+colours = ['#f5c5cb', '#f5c7a9', '#e2f0cb', '#f7e5ad', '#ff9aa2', '#d8f7ad', 
+          '#aadef7', '#a3a8f0', '#a4b8ac', '#f09ec8', '#d3a8f0', '#55cbcd','#c9f7e0', '#c9d2f7']
+#bar chart showing overall flights categorized by short, medium and long haul 
+# and seperated by colour for each airline which is visible with a hover 
+
+#short haul: less than 807 (average of 1,100-1,500 km)
+#mid haul: 807 - 2765
+#long haul: more than 2765
+#distance is in miles
+#scheduled time is in minutes
+
+#-------getting the data how I want it-------------
+#-------adding new column with haul typ for easier categorization-------
+df_haul_by_distance = df[['DISTANCE','AIRLINE']]
+
+conditions=[(df_haul_by_distance['DISTANCE'] < 807),(df_haul_by_distance['DISTANCE'] >= 807)&(df_haul_by_distance['DISTANCE'] <= 2765),(df_haul_by_distance['DISTANCE'] > 2765)]
+values = ['short haul','mid haul','long haul']
+
+df_haul_by_distance['HAUL_TYP'] = np.select(conditions,values)
+
+# print(df_haul_by_distance['HAUL_TYP'].unique())
+# print(df_haul_by_distance[df_haul_by_distance['HAUL_TYP']== 'short haul']['HAUL_TYP'])
+
+df_haul_by_distance = df_haul_by_distance.sort_values(by ='HAUL_TYP', ascending=True)
+
+df_haul_flights_grouped = df_haul_by_distance.groupby(['AIRLINE', 'HAUL_TYP'])['HAUL_TYP'].count()
+df_haul_flights_grouped = df_haul_flights_grouped.unstack()
+df_haul_flights_grouped = df_haul_flights_grouped.iloc[::-1]
+# print(df_haul_flights_grouped.index)
+
+df_haul_by_distance_airline = df_haul_by_distance.sort_values(by= ['AIRLINE', 'HAUL_TYP'])
+# print(df_haul_by_distance_airline)
+# print(df_haul_by_distance_airline['HAUL_TYP'].unique())
+
+# print(df_haul_by_distance['HAUL_TYP'].count())
+print(df_haul_by_distance[df_haul_by_distance['HAUL_TYP'] == 'long haul']['HAUL_TYP'].count())
+
+#------overall haul types-----------
+fig = go.Figure()
+bar = go.Bar(x = df_haul_by_distance['HAUL_TYP'].unique(), 
+             y = [df_haul_by_distance[df_haul_by_distance['HAUL_TYP'] == 'long haul']['HAUL_TYP'].count(), df_haul_by_distance[df_haul_by_distance['HAUL_TYP'] == 'mid haul']['HAUL_TYP'].count(), df_haul_by_distance[df_haul_by_distance['HAUL_TYP'] == 'short haul']['HAUL_TYP'].count()],
+            )            
+fig.add_trace(bar)
+fig.show()
+#if there is time: add number of flights to bar chart that changed depending on how many are selected
+
+#---------specific airlines and haul types------------
+fig = go.Figure()
+
+for i in range(len(df_haul_flights_grouped)):
+    bar_short = go.Bar(x = df_haul_by_distance_airline['HAUL_TYP'].unique(),
+                          y= df_haul_flights_grouped.loc[df_haul_flights_grouped.index[i]],
+                          name = df_haul_flights_grouped.index[i], marker= dict(color= colours[i]))
+    fig.add_trace(bar_short)
+
+# fig.update_traces(hoverinfo = )
+fig.update_layout(barmode = 'stack', title = 'Flights categorized by their distance and airline', title_font_size= 25,
+                  title_font_family= 'Arial Black', legend_title_font_family = 'Arial Black',
+                  legend_title_text = 'Airlines', coloraxis= dict(colorbar= dict(title= 'Legend Title')))
+fig.show()
+
+
+
+
+
+# for i in df_haul_by_distance:
+#     if (df['DISTANCE'] < 807):
+#         df.insert(2, 'HAUL_TYP', 'short haul')
+#     elif (df['DISTANCE'] > 2765):
+#         df.insert(2, 'HAUL_TYP', 'long haul')
+#     else: df.insert(2, 'HAUL_TYP', 'mid haul')
+#tried with for loop, didn't work, tried np.where(), but only works for 2 values
+
+# df_haul_by_distance['HAUL_TYP']= 'short haul'
+# print(df_haul_by_distance["HAUL_TYP"])
+
+#print(df_haul_by_distance)
+
+
+# for i in df_haul_by_distance:
+    # if ((df_haul_by_distance['DISTANCE'][i] > 2765).any()):
+    #     print()
+    #     df_haul_by_distance = df_haul_by_distance['HAUL_TYP'].replace('short haul', 'long haul') 
+    # elif ((807 <= df_haul_by_distance['DISTANCE'] <= 2765).any()):
+
+# df_haul_by_distance["HAUL_TYP"] = df_haul_by_distance['HAUL_TYP'].replace('short haul', 'mid haul')
+# df_haul_by_distance[df_haul_by_distance['DISTANCE'] < 807]["HAUL_TYP"] = "short haul"
+# df_haul_by_distance[df_haul_by_distance['DISTANCE'] > 2765]['HAUL_TYP'] = df_haul_by_distance[df_haul_by_distance['DISTANCE'] > 2765]["HAUL_TYP"].replace('mid haul',"long haul")
+
+# df_haul_by_distance['HAUL_TYP'] = df_haul_by_distance['DISTANCE'].transform(lambda x: 'short haul' if x < 807  else 'mid haul')
+
+# df_haul_by_distance['HAUL_TYP'] = np.where(df['DISTANCE']< 807, 'short haul', np.NAN)
+
+# df_haul_by_distance['HAUL_TYP'] = np.where(df['DISTANCE']< 807, 'short haul', np.NAN)
+# df_haul_by_distance['HAUL_TYP'] = df_haul_by_distance.loc[df['DISTANCE']< 807, 'short haul']
+# df['HAUL_TYP'] = np.where(df['DISTANCE']> 2765, 'long haul', 'mid haul')
+# df['HAUL_TYP'] = np.where((df['DISTANCE']>= 807) & (df['DISTANCE']<= 2765), 'short haul')
+
+# conditions = [
+#     (df_haul_by_distance['DISTANCE'] < 807),
+#     (df_haul_by_distance['DISTANCE'] >= 807) & (df_haul_by_distance['DISTANCE'] <= 2765),
+#     (df_haul_by_distance['DISTANCE'] > 2765)]
+# choices = ['short haul', 'mid haul', 'long haul']
+# df_haul_by_distance['HAUL_TYP'] = np.select(conditions, choices)
+
+
+# print(df_haul_by_distance['HAUL_TYP'] == 'short haul')
+# df_mid_haul_by_distance = df[807 <= df['DISTANCE']<= 2765]
---
+++ boxplot_animated.py
@@ -0,0 +1,70 @@
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+from plotly.subplots import make_subplots
+from datetime import datetime
+
+df = pd.read_csv('https://gist.githubusercontent.com/florianeichin/cfa1705e12ebd75ff4c321427126ccee/raw/c86301a0e5d0c1757d325424b8deec04cc5c5ca9/flights_all_cleaned.csv', sep=',')
+
+#boxplot with average delay per hour
+#frames with for loop
+
+df_boxplot_destination = df[['SCHEDULED_DESTINATION', 'DESTINATION_DELAY' ]]
+substring_hour_delay = []
+for i in range(len(df_boxplot_destination)):
+    substring_hour_delay.append(df_boxplot_destination['SCHEDULED_DESTINATION'][i][9:13:1])
+    #------since its on two days, i need the reference of what day it is, so the time on jan 1st will be 100, and on 2nd 200 to differentiate between the hours
+
+df_boxplot_destination['DESTINATION_HOUR_DELAY'] = substring_hour_delay
+df_boxplot_destination['DESTINATION_HOUR_DELAY'] = df_boxplot_destination['DESTINATION_HOUR_DELAY'].str.replace(' ', '')
+df_boxplot_destination['DESTINATION_HOUR_DELAY'] = df_boxplot_destination['DESTINATION_HOUR_DELAY'].astype('int64')
+df_boxplot_destination = df_boxplot_destination.sort_values(by= 'SCHEDULED_DESTINATION')
+
+#print(df_boxplot_destination.groupby(['DESTINATION_HOUR_DELAY']).count())
+
+print(df_boxplot_destination.dtypes)
+
+hours= []
+for i in range(len(df_boxplot_destination)):
+    hours.append(df_boxplot_destination['DESTINATION_HOUR_DELAY'][i])
+hours = sorted(set(hours))
+
+hour_groups = []
+for hour in hours:
+    hour_groups.append(df_boxplot_destination[df_boxplot_destination['DESTINATION_HOUR_DELAY']== hour])
+
+print(hour_groups)
+#array of dataframes
+        
+
+#print(substring_hour_delay)
+
+
+# df_boxplot_destination['SCHEDULED_DESTINATION'] = pd.to_datetime(df_boxplot_destination['SCHEDULED_DESTINATION'], format= '%Y-%m-%d %H:%M:%S')
+# print(df_boxplot_destination)
+# print(df_boxplot_destination['SCHEDULED_DESTINATION'].dtype)
+
+# df_boxplot_destination_test = df_boxplot_destination.groupby(['SCHEDULED_DESTINATION']).count()
+# print(df_boxplot_destination_test)
+
+# for i in range(len(df_boxplot_destination)):
+#     if (datetime.strptime('2015-01-01 00:00:00', format= '%Y-%m-%d %H:%M:%S') <= df_boxplot_destination['SCHEDULED_DESTINATION'][i]) & (df_boxplot_destination['SCHEDULED_DESTINATION'][i] <=datetime.strptime('2015-01-01 04:00:00', format= '%Y-%m-%d %H:%M:%S') ):
+#         print("HALLO")
+
+data = []
+frame_test= []
+fig = go.Figure()
+boxplot_3= go.Box(x= hour_groups[0]['DESTINATION_DELAY'])
+for i in range(len(hour_groups)):
+    frame_test.append(go.Frame(data = (go.Box(x= hour_groups[i]['DESTINATION_DELAY']))))
+print(frame_test)
+fig.add_trace(boxplot_3)
+fig.frames = frame_test
+#fig.add_trace(frame_test)
+fig.update_layout(updatemenus=[dict(
+            type="buttons",
+            buttons=[dict(label="Play",
+                          method="animate",
+                          args=[None]),
+                    ])]) #add a slider
+fig.show()
